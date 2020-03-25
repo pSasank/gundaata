18eef82aeee553b1a33be77e91e58a81f5dad276
Pruthvi Mandava <mpruthvisasank@gmail.com>
2020-03-26 01:46:30 +0530

    upddate 1.2 high score bet incrementer and bug fixes

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
 var userpot = 1000;
 var bets= {"1":0,"2":0,"3":0,"4":0,"5":0,"6":0};
 window.onload = function(){
-    document.querySelector('.betIncrementer').addEventListener("click",function(el){
-        console.log(el.target.className);
-        console.log(el.target.parentNode);
+    document.querySelectorAll('.betIncrementer').forEach(function(i){
+    i.addEventListener("click",function(el){
+        incrementBets(el);
     });
+    });
+    setHighScore();
 }   
 function playGame(){
-    document.querySelector(".die1").style.backgroundImage = "";
-    document.querySelector(".die2").style.backgroundImage = "";
-    Object.keys(bets).forEach(i=>{
-        var query = "input.betValue_"+i;
-        var betValue = document.querySelector(query).value;
-        bets[i] = Number(betValue);
-    });
+    if(!areBetsValid()) return;
     var totalBets = betsTotal(bets);
-    if(userpot < totalBets){
-    document.querySelector('.newsReel').innerHTML = "<h3>You don't have enough cash! Please reduce the bet</h3>";
-    return;    
-    }
-    else userpot = userpot - totalBets;
+    userpot = userpot - totalBets;
     calculateWinnings(playDice(), bets);
+
 }
 function betsTotal(bets){
     return Object.values(bets).reduce((acc,cur)=>acc+cur); //combo of all bets;
@@ -49,16 +42,32 @@ function calculateWinnings(diceResults, bets){
     var dice2 = diceResults[1];
     var winnings = (bets[dice1] * 2) + (bets[dice2] * 2);
     userpot = userpot + winnings;
+    if(winnings){
+        document.querySelector('.newsReel').innerHTML = `<h3>You won ${winnings}</h3>`;    
+        if(window && window.localStorage){
+            if(!window.localStorage["highScore"] || window.localStorage["highScore"] < userpot) window.localStorage["highScore"] = winnings;
+        }
+    }
     document.querySelector('.userpot').innerText = userpot;
     clearBets();
     if(userpot == 0) gameOver();
 }
+function setHighScore(){
+    if(window && window.localStorage && window.localStorage["highScore"]) document.querySelectorAll('.highScore').forEach(function(i){
+i.innerText = window.localStorage["highScore"];
+    })
+}
 function gameOver(){
+    /*
     document.querySelector('.newsReel').innerHTML = "<h2>Game Over</h2>";
     document.querySelector('.playgame.btn').setAttribute("disabled","disabled");
+    */
+   setHighScore();
+   showOverlay();
 }
 function startOver(){
     userpot = 1000;
+    setHighScore();
     clearBets();
     document.querySelector('.userpot').innerText = userpot;
     document.querySelector('.newsReel').innerHTML = "<h2>New Game</h2>";
@@ -67,12 +76,46 @@ function startOver(){
 function clearBets(){
     Object.keys(bets).forEach(i=>{
         var query = "input.betValue_"+i;
-        document.querySelector(query).value = "0";
-        
+        document.querySelector(query).value = "";
     });
 }
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+function incrementBets(el){
+    if(!areBetsValid()) return;
+    var amount = el.target.attributes["increment"]["value"];
+    var card = el.target.attributes["card"]["value"];
+    var inputField = ".betValue_" + card;
+    document.querySelector(inputField).value = Number(document.querySelector(inputField).value) + Number(amount);
+    if(!areBetsValid()){
+    document.querySelector('.newsReel').innerHTML = "<h3>You don't have enough cash to increase the bet!</h3>";
+        document.querySelector(inputField).value = Number(document.querySelector(inputField).value) - Number(amount);
+        if(document.querySelector(inputField).value == 0) document.querySelector(inputField).value = "";
+    }
+}
+function areBetsValid(){
+    Object.keys(bets).forEach(i=>{
+        var query = "input.betValue_"+i;
+        var betValue = document.querySelector(query).value;
+        bets[i] = Number(betValue);
+    });
+    var totalBets = betsTotal(bets);
+    if(isNaN(totalBets)){
+        document.querySelector('.newsReel').innerHTML = "<h3>Please enter valid bet Amount</h3>";
+        return false;    
+    }
+    if(userpot < totalBets){
+    document.querySelector('.newsReel').innerHTML = "<h3>You don't have enough cash! Please reduce the bet</h3>";
+    return false;    
+    }
+    return true;
+}
+function showOverlay(){
+    document.querySelector(".overlay").style.display = "block";
+}
+function hideOverlay(){
+    document.querySelector(".overlay").style.display = "none";
 }
